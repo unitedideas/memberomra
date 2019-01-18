6f7d436e5e862627834ff562de0dee4716527c20
Shane Cheek <unitedideas@gmail.com>
2019-01-17 17:33:25 -0800

    creating django form

--- members/forms.py
+++ members/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.forms import ModelForm
+from members.models import Rider
 from django.contrib.auth.forms import AuthenticationForm
 from django.forms.widgets import PasswordInput, TextInput
 
@@ -6,3 +8,12 @@ from django.forms.widgets import PasswordInput, TextInput
 class CustomAuthForm(AuthenticationForm):
     username = forms.CharField(widget=TextInput(attrs={'placeholder': 'Username', 'class': "inputs_fields"}))
     password = forms.CharField(widget=PasswordInput(attrs={'placeholder': 'Password', 'class': "inputs_fields"}))
+
+
+class AddMemberForm(ModelForm):
+    class Meta:
+        model = Rider
+        fields = '__all__'
+
+
+add_form = AddMemberForm()

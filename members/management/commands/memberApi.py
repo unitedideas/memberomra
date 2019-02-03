ffe12656d4520d2242bbed4d48062ac1743d973b
Unitedideas <unitedideas@gmail.com>
2019-02-03 15:19:38 -0800

    added api to management/ command directory to access the rider model

--- members/management/commands/memberApi.py
+++ members/management/commands/memberApi.py
@@ -18,52 +18,59 @@ from django.core.management.base import BaseCommand
 import json
 import requests
 from memberomra.secrets import OMRA_API_KEY
-from members.models import Rider
+
+
+# from members.models import Rider
+
+
+def add_members():
+    """connect to squarespace API"""
+
+    baseUrl = 'https://api.squarespace.com/1.0/commerce/orders'
+
+    headers = {
+        'Authorization': OMRA_API_KEY,
+    }
+
+    params = (
+        ('fulfillmentStatus', 'PENDING'),
+    )
+
+    # First Name
+    # Last Name
+    # Current Rider OMRA Number
+    # Rider Age *
+    # Rider Birth Date *
+    # MC Brand and Model
+    # Email Address *
+    # Class *
+
+    response = requests.get('https://api.squarespace.com/1.0/commerce/orders/', headers=headers, params=params)
+    pendingMembers = response.json()
+    # customizations
+    custom_form_data = pendingMembers['result']
+    for each in custom_form_data:
+        for rider in each['lineItems']:
+            key_list = []
+            data_list = []
+            try:
+                for eachItem in rider['customizations']:
+                    for value in eachItem:
+                        if value == 'label':
+                            key_list.append(eachItem[value])
+                        if value == 'value':
+                            data_list.append(eachItem[value])
+                doc_zip = dict(zip(key_list, data_list))
+                r = json.dumps(doc_zip)
+                print(r)
+
+            except TypeError:
+                print('TypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeError')
+
+
+add_members()
 
 
 class Command(BaseCommand):
-    """connect to squarespace API"""
-
     def handle(self, *args, **options):
-        baseUrl = 'https://api.squarespace.com/1.0/commerce/orders'
-
-        headers = {
-            'Authorization': OMRA_API_KEY,
-        }
-
-        params = (
-            ('fulfillmentStatus', 'PENDING'),
-        )
-
-        # First Name
-        # Last Name
-        # Current Rider OMRA Number
-        # Rider Age *
-        # Rider Birth Date *
-        # MC Brand and Model
-        # Email Address *
-        # Class *
-
-        response = requests.get('https://api.squarespace.com/1.0/commerce/orders/', headers=headers, params=params)
-        pendingMembers = response.json()
-        # customizations
-        custom_form_data = pendingMembers['result']
-        for each in custom_form_data:
-            for rider in each['lineItems']:
-                print(rider)
-                key_list = []
-                data_list = []
-                try:
-                    for eachItem in rider['customizations']:
-                        for value in eachItem:
-                            if value == 'label':
-                                key_list.append(eachItem[value])
-                            if value == 'value':
-                                data_list.append(eachItem[value])
-                    doc_zip = dict(zip(key_list, data_list))
-                    r = json.dumps(doc_zip)
-                    print(r)
-
-                except TypeError:
-                    print('TypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeErrorTypeError')
-                print('--------------------')
+        add_members()
